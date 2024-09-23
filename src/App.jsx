f91e1c639e5d969bb7aefb8190827e181dd59e27
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-09-23 13:13:24 +0300

    fix positioning

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,24 @@ import transactions from "./transactions.json";
 function App() {
   return (
     <div className="mainContent">
-      <h2>Task 1</h2>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-
-      <h2>Task 2</h2>
-      <FriendList friends={friends} />
-
-      <h2>Task 3</h2>
-      <TransactionHistory items={transactions} />
+      <div className="section">
+        <h2>Task 1</h2>
+        <Profile
+          name={userData.username}
+          tag={userData.tag}
+          location={userData.location}
+          image={userData.avatar}
+          stats={userData.stats}
+        />
+      </div>
+      <div className="section">
+        <h2>Task 2</h2>
+        <FriendList friends={friends} />
+      </div>
+      <div className="section">
+        <h2>Task 3</h2>
+        <TransactionHistory items={transactions} />
+      </div>
     </div>
   );
 }
